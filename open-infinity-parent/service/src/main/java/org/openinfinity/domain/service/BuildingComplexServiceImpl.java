55b87b9d919a2c8174171e6d619a4d8b8bd7863d
Mohammad Bakharzy <mohammad.bakharzy@helsinki.fi>
2013-05-14 19:49:51 +0300

    adding test folder in service layer

---
+++ open-infinity-parent/service/src/main/java/org/openinfinity/domain/service/BuildingComplexServiceImpl.java
@@ -0,0 +1,39 @@
+package org.openinfinity.domain.service;
+
+import java.util.Collection;
+
+import org.openinfinity.domain.entity.BuildingComplex;
+
+public class BuildingComplexServiceImpl implements BuildingComplexService {
+
+	@Override
+	public String createBuildingComplex(String address) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteBuildingComplex(BuildingComplex buildingComplex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<BuildingComplex> queryByAddress(String address) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateBuildingComplexAddress(String address) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Collection<BuildingComplex> listAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
